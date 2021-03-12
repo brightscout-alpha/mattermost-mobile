16c674e9ad62714b3954fdeed2176550091e4c00
Manoj <manoj@brightscout.com>
2021-03-12 15:38:52 +0530

    WIP : Unit testing for custom status feature Added unit tests for custom status emoji component Added unit tests for custom status text component Added unit tests for clear button component Added unit tests for custom status suggestion component Added unit tests for custom status modal component Updated unit tests for settings_sidebar Updated unit tests for drawer item

---
+++ app/components/custom_status/custom_status_emoji.test.tsx
@@ -0,0 +1,71 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+import {shallow} from 'enzyme';
+import React from 'react';
+
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+
+import * as CustomStatusSelectors from '@selectors/custom_status';
+
+import CustomStatusEmoji from '@components/custom_status/custom_status_emoji';
+import Emoji from '@components/emoji';
+
+jest.mock('@selectors/custom_status');
+
+describe('components/custom_status/custom_status_emoji', () => {
+    const mockStore = configureStore();
+    const store = mockStore({});
+
+    const getCustomStatus = () => {
+        return {
+            emoji: 'calendar',
+            text: 'In a meeting',
+        };
+    };
+    (CustomStatusSelectors.makeGetCustomStatus as jest.Mock).mockReturnValue(getCustomStatus);
+    (CustomStatusSelectors.isCustomStatusEnabled as jest.Mock).mockReturnValue(true);
+    it('should match snapshot', () => {
+        const wrapper = shallow(
+            <Provider store={store}>
+                <CustomStatusEmoji/>
+            </Provider>,
+        );
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+
+    it('should match snapshot with props', () => {
+        const wrapper = shallow(
+            <Provider store={store}>
+                <CustomStatusEmoji
+                    emojiSize={34}
+                />
+            </Provider>,
+        );
+
+        expect(wrapper.getElement()).toMatchSnapshot();
+    });
+
+    it('should not render when EnableCustomStatus in config is false', () => {
+        (CustomStatusSelectors.isCustomStatusEnabled as jest.Mock).mockReturnValue(false);
+        const wrapper = shallow(
+            <Provider store={store}>
+                <CustomStatusEmoji/>
+            </Provider>,
+        );
+
+        expect(wrapper.containsMatchingElement(<Emoji/>)).toBeFalsy();
+    });
+
+    it('should not render when getCustomStatus returns null', () => {
+        (CustomStatusSelectors.isCustomStatusEnabled as jest.Mock).mockReturnValue(true);
+        (CustomStatusSelectors.makeGetCustomStatus as jest.Mock).mockReturnValue(() => null);
+        const wrapper = shallow(
+            <Provider store={store}>
+                <CustomStatusEmoji/>
+            </Provider>,
+        );
+
+        expect(wrapper.containsMatchingElement(<Emoji/>)).toBeFalsy();
+    });
+});
