c9a1ed839ab3e712e413f0f20a1bbe04f1ec5919
Manoj <manoj@brightscout.com>
2021-04-30 19:11:24 +0530

    Review fixes Sorted the import order in all files Modified removeRecentCustomStatus action not to use bindClientFunction Added types for custom status client functions Added containerSize prop in clear button component Removed the check for custom status enabled from custom status emoji and added it in post_header, channel_title, channel_item Modified getCustomStatusSelector to return undefined if custom status does not exist Modified settings_sidebar, channel_info, channel_info_header, custom_status_modal, user_profile to handle undefined custom status Removed the text wrapper from the custom status emoji in channel_info_header Added check for identifying the navigation button pressed in custom status modal

--- app/selectors/custom_status.ts
+++ app/selectors/custom_status.ts
@@ -1,20 +1,25 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import {GlobalState} from '@mm-redux/types/store';
-import {UserCustomStatus} from '@mm-redux/types/users';
 
 import {createSelector} from 'reselect';
-import {getCurrentUser, getUser} from '@mm-redux/selectors/entities/users';
-import {get} from '@mm-redux/selectors/entities/preferences';
+
 import {Preferences} from '@mm-redux/constants';
 import {getConfig} from '@mm-redux/selectors/entities/general';
+import {get} from '@mm-redux/selectors/entities/preferences';
+import {getCurrentUser, getUser} from '@mm-redux/selectors/entities/users';
+import {UserCustomStatus} from '@mm-redux/types/users';
+import {GlobalState} from '@mm-redux/types/store';
 
 export function makeGetCustomStatus(): (state: GlobalState, userID?: string) => UserCustomStatus {
     return createSelector(
         (state: GlobalState, userID?: string) => (userID ? getUser(state, userID) : getCurrentUser(state)),
         (user) => {
             const userProps = user?.props || {};
-            return userProps.customStatus && JSON.parse(userProps.customStatus);
+            if (userProps.customStatus) {
+                return JSON.parse(userProps.customStatus) || undefined;
+            }
+
+            return undefined;
         },
     );
 }
