cb66bca0b4de3ed7818b7957672acfbfee063405
manojmalik20 <manoj@brightscout.com>
2021-05-12 10:30:19 +0530

    added snapshot test cases

---
+++ app/components/custom_status/custom_status_expiry.test.tsx
@@ -0,0 +1,28 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+
+import * as PreferenceSelectors from '@mm-redux/selectors/entities/preferences';
+import {renderWithReduxIntl} from 'test/testing_library';
+import CustomStatusExpiry from './custom_status_expiry';
+import Preferences from '@mm-redux/constants/preferences';
+
+jest.mock('@mm-redux/selectors/entities/preferences');
+
+describe('components/custom_status/custom_status_emoji', () => {
+    const baseProps = {
+        theme: Preferences.THEMES.default,
+        time: new Date(),
+    };
+
+    (PreferenceSelectors.getBool as jest.Mock).mockReturnValue(false);
+    it('should match snapshot', () => {
+        const wrapper = renderWithReduxIntl(
+            <CustomStatusExpiry
+                {...baseProps}
+            />,
+        );
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+});
